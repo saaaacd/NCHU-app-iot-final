34e5123d07dcef86a41885ce7d0aef344d26ac22
lly <dbjenny555@gmail.com>
2025-09-21 04:19:15 +0800

    Initial Commit

---
+++ appTests/appTests.swift
@@ -0,0 +1,17 @@
+//
+//  appTests.swift
+//  appTests
+//
+//  Created by 劉李陽 on 2025/9/21.
+//
+
+import Testing
+@testable import app
+
+struct appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
